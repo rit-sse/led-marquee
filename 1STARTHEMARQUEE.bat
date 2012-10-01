3710026d4a6b90bf22f8bff89d18b8e18048a9ee
Michael Timbrook <timbrook480@gmail.com>
2012-10-01 09:33:13 -0400

    Working with plugable drivers (needs tested on windows)
    http://plugable.com/drivers/prolific/

--- 1STARTHEMARQUEE.bat
+++
@@ -1,6 +0,0 @@
-ipconfig /release
-ipconfig /renew
-w32tm /resync
-cd /D C:\Users\touched\Desktop\led_marquee\
-cls
-c:\Python26\python.exe marquee.py
--- webserver.py
+++ webserver.py
@@ -10,7 +10,8 @@ from spamFilter import SpamFilter
 ## TODO: it really sucks that we need to use a global variable
 marquee = MarqueeWriter("/dev/cu.usbserial", 115200)
 
-        #"/dev/ttyUSB0"         on linux
+        #"comm3"               on windows try using http://plugable.com/drivers/prolific/ (not tested yet)
+        #"/dev/cu.usbserial"    on mac w/ http://plugable.com/drivers/prolific/
 
 
 spam = SpamFilter(3,4)

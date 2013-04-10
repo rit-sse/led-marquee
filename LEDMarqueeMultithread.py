963f45ead49cbec60d67b1d930569f15cde55bdb
Corban Mailloux <github@corb.co>
2013-04-10 10:55:27 -0400

    Made a multithreaded version of the main program to test

---
+++ LEDMarqueeMultithread.py
@@ -0,0 +1,51 @@
+from inputs import InputClock
+from inputs import InputConsole
+from inputs import InputWeather
+import time, threading, queue
+
+def ledMarquee():
+	"""
+	Main function for LEDMarquee. Sends properly formatted
+	text to hardware for display on the Marquee.
+
+	Current state: Printing as simulation.
+	"""
+
+	# Which files to get input from
+	printList = [InputConsole, InputClock, InputWeather]
+
+	# Minimum time, in seconds, between updates
+	timeOut = 2
+
+	# Display queue
+	q = queue.Queue()
+
+	t = threading.Thread(target=updateThread, args=(q, printList, timeOut))
+	t.daemon = True
+	t.start()
+
+	while(True):
+		
+		s = q.get()
+		print(s)
+
+
+def updateThread(q, inList, timeOut):
+	prevStrDict = {}
+	for i in inList:
+		prevStrDict[i] = i.get()
+
+	# Iterates through all given commands.
+	while (True):
+		for i in inList:
+			printStr = i.get()
+			time.sleep(timeOut)
+			# If the .get() command returns nothing or hasn't changed, ignore it.
+			if (printStr and prevStrDict[i] != printStr):
+				# If it has changed, update the dict and queue.
+				prevStrDict[i] = printStr
+				q.put(printStr)
+
+
+
+ledMarquee()

b03ba3cc0be0a6ed5c93d50d121f3e85175cc85f
Shayde Nofziger <shaydenofziger@gmail.com>
2013-04-10 17:33:47 -0400

    Commented out the _state_ weather type to make it more streamlined. Lets decide which we like better.

--- inputs/InputWeather.py
+++ inputs/InputWeather.py
@@ -15,12 +15,11 @@ def get():
 
 	if weather_info is not None:
 		temp = str(k_to_f(weather_info['main']['temp']))
-		state = str(weather_info['weather'][0]['main'])
+		#state = str(weather_info['weather'][0]['main'])
 		desc = str(weather_info['weather'][0]['description'])
-		weather_string = temp + " degrees, " + state + " (" + desc + ")"
+		weather_string = temp + " degrees, " + desc
 	
 	return weather_string
-
 
 def k_to_f(kelvin):
 	"""

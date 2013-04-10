93e69a4ac139e1b5ac350cbc0c816050519f8b6b
Shayde Nofziger <shaydenofziger@gmail.com>
2013-04-10 09:02:57 -0400

    Updated LEDMarquee with documentation and changed function name to better represent its contents

--- LEDMarquee.py
+++ LEDMarquee.py
@@ -3,9 +3,16 @@ from inputs import InputConsole
 from inputs import InputWeather
 import time
 
-def cyclePrint():
-	printList = [InputConsole, InputClock, InputWeather]
+def ledMarquee():
+	"""
+    Main function for LEDMarquee. Sends properly formatted
+    text to hardware for display on the Marquee.
+
+    Current state: Printing as simulation.
+    """
+    printList = [InputConsole, InputClock, InputWeather]
 	while(True):
+        #Iterates through all given commands, printing .get() statements.
 		for i in printList:
 			printStr = i.get()
 			if (printStr):
@@ -13,4 +20,4 @@ def cyclePrint():
 				print(printStr)
 
 
-cyclePrint()
+ledMarquee()

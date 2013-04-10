fb0cde62697a14b227dacd3f4381044d391cf668
Shayde Nofziger <shaydenofziger@gmail.com>
2013-04-10 16:55:38 -0400

    Added Momentum support with help from the Momentum team. Updated LEDMarquee to print next event.

--- LEDMarquee.py
+++ LEDMarquee.py
@@ -1,27 +1,28 @@
 from inputs import InputClock
 from inputs import InputConsole
 from inputs import InputWeather
+from inputs import InputMomentum
 from filters import profanityfilter
 import time
 
 def ledMarquee():
-	"""
-	Main function for LEDMarquee. Sends properly formatted
-	text to hardware for display on the Marquee.
+  """
+  Main function for LEDMarquee. Sends properly formatted
+  text to hardware for display on the Marquee.
 
-	Current state: Printing as simulation.
-	"""
-	printList = [InputConsole, InputClock, InputWeather]
+  Current state: Printing as simulation.
+  """
+  printList = [InputConsole, InputClock, InputWeather, InputMomentum]
 
-	pro = profanityfilter.ProfanityFilter()
+  pro = profanityfilter.ProfanityFilter()
 
-	while(True):
-		#Iterates through all given commands, printing .get() statements.
-		for i in printList:
-			printStr = i.get()
-			if (printStr):
-				time.sleep(2)
-				print(pro.replaceProfanity(printStr))
+  while(True):
+    #Iterates through all given commands, printing .get() statements.
+    for i in printList:
+      printStr = i.get()
+      if (printStr):
+        time.sleep(2)
+        print(pro.replaceProfanity(printStr))
 
 
 ledMarquee()
---
+++ inputs/InputMomentum.py
@@ -0,0 +1,20 @@
+import urllib.request
+import json
+
+def get():
+    """
+    Returns properly formatted string for the next event
+    taking place in the SSE.
+    """
+    next_event = ""
+    event_url = "https://sse.se.rit.edu/events/current.json"
+    request = urllib.request.urlopen(event_url)
+    event_info = json.loads(request.read().decode("utf-8"))
+    request.close
+
+    if event_info is not None:
+        next_event = str(event_info['short_name'])
+
+    return next_event
+get()
+

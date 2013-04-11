16294df7378cfa2edb77a0625aef8e3a56ae659d
Corban Mailloux <github@corb.co>
2013-04-11 03:18:54 -0400

    Added i2c code

--- LEDMarquee.py
+++ LEDMarquee.py
@@ -1,3 +1,5 @@
+import quick2wire.i2c as i2c
+
 from inputs import InputClock
 from inputs import InputConsole
 from inputs import InputWeather
@@ -6,26 +8,43 @@ from inputs import InputEmail
 from filters import profanityfilter
 import time
 
+
+
 def ledMarquee():
-  """
-  Main function for LEDMarquee. Sends properly formatted
-  text to hardware for display on the Marquee.
+	"""
+	Main function for LEDMarquee. Sends properly formatted
+	text to hardware for display on the Marquee.
 
-  Current state: Printing as simulation.
-  """
-  printList = [InputConsole, InputClock, InputWeather, InputMomentum, InputEmail]
+	Current state: Printing as simulation.
+	"""
+	printList = [InputConsole, InputClock, InputWeather, InputMomentum, InputEmail]
 
-  pro = profanityfilter.ProfanityFilter()
+	pro = profanityfilter.ProfanityFilter()
 
-  while(True):
-    #Iterates through all given commands, printing .get() statements.
-    for i in printList:
-      printStr = i.get()
-      if (printStr):
-        time.sleep(2)
-        if (i.isFiltered()):
-          printStr = pro.replaceProfanity(printStr)
-        print(printStr)
+	while(True):
+		#Iterates through all given commands, printing .get() statements.
+		testSend("this")
+		for i in printList:
+			printStr = i.get()
+			if (printStr):
+				time.sleep(2)
+				if (i.isFiltered()):
+					printStr = pro.replaceProfanity(printStr)
+				sendArd(printStr)
+				print(printStr)
 
 
+def sendArd(sendStr):
+	address = 0x04
+	print("test")
+	byteList = []
+	for i in sendStr:
+		byteList.append(ord(i))
+	byteList.append(0x0A)
+
+	with i2c.I2CMaster() as bus:    
+		bus.transaction(
+		i2c.writing(address, bytes(byteList)))
+
+send("test")
 ledMarquee()

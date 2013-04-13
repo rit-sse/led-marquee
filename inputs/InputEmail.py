a9cff19d7b76e4bc56a29470c26de28b198dc99d
Shayde Nofziger <shaydenofziger@gmail.com>
2013-04-13 14:55:15 -0400

    Cleaned up formatting and comment issues. Removed old code.

--- inputs/InputEmail.py
+++ inputs/InputEmail.py
@@ -2,7 +2,10 @@ import imaplib, email, configparser
 
 
 def get():
-
+	"""
+	Returns the properly formatted String grabbed from Email to display
+	as SMS output to reader.
+	"""
 	config = configparser.ConfigParser()
 	config.read('config.ini')
 
@@ -25,29 +28,28 @@ def get():
 	id_list = ids.split() # ids is a space separated string
 	if len(id_list) > 0:
 		latest_email_id = id_list[-1] # get the latest
-		 
-		result, data = mail.fetch(latest_email_id, "(RFC822)") # fetch the email body (RFC822) for the given ID
-		 
-		raw_email = data[0][1] # here's the body, which is raw text of the whole email
-		# including headers and alternate payloads
 
-		#if ("txt.voice.google.com" in raw_email):
+		# fetch the email body (RFC822) for the given ID
+		result, data = mail.fetch(latest_email_id, "(RFC822)")
+		"""
+		here's the body, which is raw text of the whole email
+		including headers and alternate payloads
+		""" 
+		raw_email = data[0][1] 
+
 		msg = email.message_from_bytes(raw_email) 
-		#print(msg)
+		
 
 		msgStr = msg.__str__()
 
 		smsBeginIndex = msgStr.find("format=flowed; delsp=yes")
 		if (smsBeginIndex != -1):
 			msgContent = msgStr[(smsBeginIndex + 25) :].strip().splitlines()
-			return (" ".join(msgContent).replace("-- Sent using SMS-to-email. Reply to this email to text the sender back and   save on SMS fees. https://www.google.com/voice/", ""))
-			#print(msgContentStr)
-
-
-
-		#print(raw_email)
-	else:
-		print("No mail.")
+			return (" ".join(msgContent).replace("-- Sent using SMS-to-email."
+					" Reply to this email to text the sender back and"
+					"   save on SMS fees. https://www.google.com/voice/", ""))
+		else:
+			print("No mail.")
 
 def isFiltered():
 	"""

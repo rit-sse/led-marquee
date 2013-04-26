f2463c4528536327a0964be67391e6c04c1bed39
Corban Mailloux <github@corb.co>
2013-04-25 21:57:08 -0400

    Update to ledMarquee arduino code

--- Arduino/current-code/ledMarquee/ledMarquee.ino
+++ Arduino/current-code/ledMarquee/ledMarquee.ino
@@ -14,11 +14,14 @@
 #include <string.h>
 #include <ctypes.h>
 #include <Wire.h>
+#include "Timer.h"
 
 // Font file from font.ino in the same directory.
 // Usage:   FONT[(ASCII value - 32)]   <-- gives a character.
-extern const char FONT[97][8];
+extern const byte FONT[97][8];
 
+Timer t;
+byte* outChar;
 
 void setup(){
 	
@@ -27,17 +30,20 @@ void setup(){
 	ports E,K,A,C, and B are the columns,
 	while port L is the rows
 	*/
-	DDRF = B11111111;
-	DDRK = B11111111;
-	DDRA = B11111111;
-	DDRC = B11111111;
-	DDRB = B11100000;
-	DDRL = B11111111;
+	DDRF = 0b11111111;
+	DDRK = 0b11111111;
+	DDRA = 0b11111111;
+	DDRC = 0b11111111;
+	DDRB = 0b11100000;
+	DDRL = 0b11111111;
+
+  t.every(1000, updateChar);
 
   // i2c setup 
   Wire.begin(4);                // join i2c bus with address #4
   Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  //Serial.begin(9600);           // start serial for output
+  outChar = lookup('H'); //(byte*)calloc(7, sizeof(byte));
 }
 
 
@@ -48,34 +54,37 @@ void receiveEvent(int howMany)
   while(Wire.available()) // While there is data to be read
   {
     char c = Wire.read(); // receive byte as a character
-    Serial.println(c);         // print the character
-    Serial.println(lookup((int) c));
+    //Serial.print(c);         // print the character
+    free(outChar);
+    outChar = lookup(c);
+    //Serial.println(*outChar, HEX);
+    //Serial.println("Freed");
   }
 }
-
-int shiftSize[] = { 32, 24, 16, 8, 0 };
 
 // Takes an int (ASCII value of a character) and looks up
 // and returns the character.
 //uint16_t* lookup(int ascii){
-char* lookup(int ascii){
-  if (!(ascii >= 32))
-    return 0;
+byte* lookup(char ascii){
+  if (ascii < 32)
+    return NULL;
   ascii -= 32;
-  //uint16_t* row = calloc(7, sizeof(uint8_t));
-  char character[8];
-  strcpy(character,FONT[ascii]);
-  /*for (int i = 0; i < 7 ; i++){
-    character[i] >>= 2; 
-    character[i] <<= shiftSize[i];
-    (*row)[i] |= character[i];
-  }
-  return row;
-  */
+  byte* character = (byte*)calloc(7, sizeof(byte));
+  memcpy(character,FONT[ascii], sizeof(byte)*7);
   return character;
 }
 
+byte charArray[] = "Hello, Corb!";
+int charIndex = 0;
+
+void updateChar(){
+  free(outChar);
+  outChar = lookup(charArray[charIndex++]);
+  charIndex %= sizeof(charArray)/sizeof(charArray[0]) - 1;
+}
+
 void loop(){
+  t.update();
   int rows = 8;
   /* 
   The default loop, this needs to stay in place in order
@@ -84,24 +93,23 @@ void loop(){
   int index = 0;
   for (int i = 1; i <= pow(rows,2); i<<=1) {
     
-    int strLen = 2;
-    uint16_t row= 0;
+    //int strLen = 2;
+    //uint16_t row= 0;
     //Serial.println(row, BIN);
-    row |= FONT[40][index] << (8 * (strLen - 1));
+    //row |= FONT[40][index] << (8 * (strLen - 1));
     //Serial.println(row, BIN);
-    row |= FONT[37][index] << 2;
+    //row |= FONT[37][index] << 2;
     //Serial.println(row, BIN);
     //row |= FONT[37][index];
     //Set your row state here
-    PORTF = row >> 8;
-    PORTK = row & 0x00FF;
+    //PORTF = row >> 8;
+    //PORTK = row & 0x00FF;
     //PORTK = FONT[37][index];
     //PORTA = FONT[44][index];
     //PORTC = FONT[44][index];
     //PORTB = FONT[47][index];
-    index++;
+    PORTF = outChar[index++];
     PORTL = ~i;
-
     delay(2);
   }
   
